44015e5e1448d66b045fe1c1b95a64e77c4888ef
Emilio <hello@emicastro.com>
2023-01-23 19:41:43 -0300

    Prints all TEXT fields to console
    Modifies the printing to only show the Text section of the csv file.
    Line by line.

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ const FILENAME: &str = "story.csv";
 fn main() {
     let content = fs::read_to_string(FILENAME).unwrap();
 
-    println!("{}", content);
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+        println!("Text: {}", result.unwrap().get(2).unwrap().trim());
+    }
 }
